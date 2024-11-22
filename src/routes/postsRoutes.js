c09458a497224a6fb6ae951a5b9e263b079acb19
Pedro Chaves <draygom@MacDraygom.local>
2024-11-22 20:37:08 -0300

    chore: add `cors` to feed the data to the front

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import multer from "multer";
+import cors from "cors";
 import {
   listAllPosts,
   postPost,
@@ -8,9 +9,14 @@ import {
 } from "../controllers/postsController.js";
 
 const upload = multer({ dest: "./uploads" });
+const corsOptions = {
+  origin: "http://localhost:8000",
+  optionsSuccessStatus: 200,
+};
 
 const routes = (app) => {
   app.use(express.json());
+  app.use(cors(corsOptions));
 
   app.get("/posts", listAllPosts);
   app.post("/posts", postPost);
